695492125e7e2d40be21667ecf4494456f799a7a
kaushal Pandit <kisore90060@gmail.com>
2022-03-20 01:02:40 +0530

    initial commited

---
+++ script.js
@@ -0,0 +1,156 @@
+var go = $("#go")[0];
+$("#box").mousedown(function () {
+  go.play();
+});
+var playM1 = $("#a_aud")[0];
+$("#box-1").mousedown(function () {
+  playM1.play();
+});
+
+var playM2 = $("#b_aud")[0];
+$("#box-2").mousedown(function () {
+  playM2.play();
+});
+
+var playM3 = $("#Bb_aud")[0];
+$("#box-3").mousedown(function () {
+  playM3.play();
+});
+
+var playM4 = $("#c_aud")[0];
+$("#box-4").mousedown(function () {
+  playM4.play();
+});
+
+var playM5 = $("#Cb_aud")[0];
+$("#box-5").mousedown(function () {
+  playM5.play();
+});
+
+var playM6 = $("#d_aud")[0];
+$("#box-6").mousedown(function () {
+  playM6.play();
+});
+
+var playM7 = $("#e_aud")[0];
+$("#box-7").mousedown(function () {
+  playM7.play();
+});
+
+var playM8 = $("#Eb_aud")[0];
+$("#box-8").mousedown(function () {
+  playM8.play();
+});
+
+var playM9 = $("#f_aud")[0];
+$("#box-9").mousedown(function () {
+  playM9.play();
+});
+
+var playM10 = $("#Fb_aud")[0];
+$("#box-10").mousedown(function () {
+  playM10.play();
+});
+
+var playM11 = $("#g_aud")[0];
+$("#box-11").mousedown(function () {
+  playM11.play();
+});
+
+var playM12 = $("#Gb_aud")[0];
+$("#box-12").mousedown(function () {
+  playM12.play();
+});
+
+// ---------------------------------------
+var playM13 = $("#a_aud")[0];
+$("#box-13").mousedown(function () {
+  playM13.play();
+});
+
+var playM14 = $("#b_aud")[0];
+$("#box-14").mousedown(function () {
+  playM14.play();
+});
+
+var playM15 = $("#Bb_aud")[0];
+$("#box-15").mousedown(function () {
+  playM15.play();
+});
+
+var playM16 = $("#c_aud")[0];
+$("#box-16").mousedown(function () {
+  playM16.play();
+});
+
+var playM17 = $("#Cb_aud")[0];
+$("#box-17").mousedown(function () {
+  playM17.play();
+});
+
+var playM18 = $("#d_aud")[0];
+$("#box-18").mousedown(function () {
+  playM18.play();
+});
+
+var playM19 = $("#e_aud")[0];
+$("#box-19").mousedown(function () {
+  playM19.play();
+});
+
+var playM20 = $("#Eb_aud")[0];
+$("#box-20").mousedown(function () {
+  playM20.play();
+});
+
+var playM21 = $("#f_aud")[0];
+$("#box-21").mousedown(function () {
+  playM21.play();
+});
+
+var playM22 = $("#Fb_aud")[0];
+$("#box-22").mousedown(function () {
+  playM22.play();
+});
+
+var playM23 = $("#g_aud")[0];
+$("#box-23").mousedown(function () {
+  playM23.play();
+});
+
+var playM24 = $("#Gb_aud")[0];
+$("#box-24").mousedown(function () {
+  playM24.play();
+});
+
+// -----------------------------
+
+var playM25 = $("#a_aud")[0];
+$("#box-25").mousedown(function () {
+  playM25.play();
+});
+
+var playM26 = $("#b_aud")[0];
+$("#box-26").mousedown(function () {
+  playM26.play();
+});
+
+var playM27 = $("#Bb_aud")[0];
+$("#box-27").mousedown(function () {
+  playM27.play();
+});
+
+var playM28 = $("#c_aud")[0];
+$("#box-28").mousedown(function () {
+  playM28.play();
+});
+
+var playM29 = $("#Cb_aud")[0];
+$("#box-29").mousedown(function () {
+  playM29.play();
+});
+
+var playM30 = $("#d_aud")[0];
+$("#box-30").mousedown(function () {
+  playM30.play();
+});
